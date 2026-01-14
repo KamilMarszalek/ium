201e53a6580acce8b6f1224222effa55cfdaf80e
KamilMarszalek <kamilmar553@gmail.com>
2026-01-14 19:01:46 +0100

    feat: add features module for listing, host, and review data

---
+++ analysis/features.py
@@ -0,0 +1,41 @@
+listing_features = [
+    "listing_id",
+    "price",
+    "room_type",
+    "accommodates",
+    "bedrooms",
+    "beds",
+    "bathrooms",
+    "bathrooms_text",
+    "amenities",
+    "property_type",
+    "instant_bookable",
+    # "segment_id",
+]
+
+night_limit_cols = [
+    "minimum_nights",
+    "maximum_nights",
+    "minimum_minimum_nights",
+    "maximum_minimum_nights",
+    "minimum_maximum_nights",
+    "maximum_maximum_nights",
+]
+
+host_features = [
+    "host_is_superhost",
+    "host_response_time",
+    "host_response_rate",
+    "number_of_reviews",
+    "host_acceptance_rate",
+]
+
+review_features = [
+    "review_scores_rating",
+    "review_scores_accuracy",
+    "review_scores_cleanliness",
+    "review_scores_checkin",
+    "review_scores_communication",
+    "review_scores_location",
+    "review_scores_value",
+]
