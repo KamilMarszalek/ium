dc44a7f8afa3b66e40d4e26f49b46458441d666b
KamilMarszalek <kamilmar553@gmail.com>
2026-01-15 23:47:13 +0100

    feat: add additional parameters to XGB_CONFIG for model tuning

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -22,5 +22,13 @@ XGB_CONFIG: dict[str, Any] = {
     "eval_metric": "logloss",
     "n_jobs": -1,
     "random_state": 42,
+    "learning_rate": 0.07284105447558731,
+    "max_depth": 5,
+    "min_child_weight": 6,
+    "subsample": 0.7023117290424328,
+    "colsample_bytree": 0.5065701578589041,
+    "reg_alpha": 4.844499375122621,
+    "reg_lambda": 4.765995161542444,
+    "scale_pos_weight": 1.0160535991758315,
 }
 LOGREG_CONFIG: dict[str, Any] = {"max_iter": 500, "n_jobs": -1}
